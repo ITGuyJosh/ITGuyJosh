f25a2dd8c46ec64794ea6975777edb47d5910bd4
Josh Evans <EvansJ92@bitbucket.org>
2015-01-23 14:59:00 +0000

    Adding contact handler and thankyou page

---
+++ contactHandler.php
@@ -0,0 +1,26 @@
+<?php
+// Contact Form Handler
+
+if(isset($_POST["submit"])){
+    $name = $_POST["yourName"];
+    $email = $_POST["yourEmail"];
+    $message = $_POST["yourMessage"];
+    
+    $to = "itguyjosh@outlook.com";
+    $subject = "Contact from website";
+    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
+    
+    mail($to, $subject, $message, $headers);
+    header("Location: http://www.itguyjosh.com/thankyou.html");
+} else {
+    echo "There has been an error on the server, please try again later";
+    
+    $to = "itguyjosh@outlook.com";
+    $subject = "ITSSS BROKEEEE";
+    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
+    
+    mail($to, $subject, $message, $headers);
+    header("Location: http://www.itguyjosh.com/");
+}
+
+?>
