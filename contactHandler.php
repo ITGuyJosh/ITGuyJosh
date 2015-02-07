6cc79150589269f937a6861d1c0b355ed2351f4b
Josh Evans <digitaljev@gmail.com>
2015-02-07 23:13:44 +0000

    adding favicon

--- contactHandler.php
+++ contactHandler.php
@@ -1,26 +1,38 @@
 <?php
+
 // Contact Form Handler
 
-if(isset($_POST["submit"])){
-    $name = $_POST["yourName"];
-    $email = $_POST["yourEmail"];
-    $message = $_POST["yourMessage"];
-    
-    $to = "itguyjosh@outlook.com";
-    $subject = "Contact from website";
-    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
-    
-    mail($to, $subject, $message, $headers);
-    header("Location: http://www.itguyjosh.com/thankyou.html");
-} else {
-    echo "There has been an error on the server, please try again later";
-    
-    $to = "itguyjosh@outlook.com";
-    $subject = "ITSSS BROKEEEE";
-    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
-    
-    mail($to, $subject, $message, $headers);
-    header("Location: http://www.itguyjosh.com/");
-}
+$name = $_POST["yourName"];
+$email = $_POST["yourEmail"];
+$message = $_POST["yourMessage"];
 
+$to = "webmaster@outlook.com";
+$subject = "Website contact - " . $name;
+$headers = "From: " . $email;
+
+mail($to, $subject, $message, $headers);
+
+header("Location: http://www.itguyjosh.com/thankyou.html");
+
+//if(isset($_POST["youName"])){
+//    $name = $_POST["yourName"];
+//    $email = $_POST["yourEmail"];
+//    $message = $_POST["yourMessage"];
+//    
+//    $to = "itguyjosh@outlook.com";
+//    $subject = "Contact from website";
+//    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
+//    
+//    mail($to, $subject, $message, $headers);
+//    header("Location: http://www.itguyjosh.com/thankyou.html");
+//} else {
+//    echo "There has been an error on the server, please try again later";
+//    
+//    $to = "itguyjosh@outlook.com";
+//    $subject = "ITSSS BROKEEEE";
+//    $headers = "From: webmaster@itguyjosh.com" . "\r\n";
+//    
+//    mail($to, $subject, $message, $headers);
+//    header("Location: http://www.itguyjosh.com/");
+//}
 ?>
